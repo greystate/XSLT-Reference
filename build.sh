bcf5211fe41dcb84a7718afe9bfce8324c29ba9d
Chriztian Steinmeier <chriztian@steinmeier.dk>
2014-02-28 00:13:27 +0100

    Also perform XInclude on build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 xsltproc --xinclude -o public/index.html build-docset.xslt elements.xml
 
 # Build the SQL to populate the docSet.dsidx SQLLite database
-xsltproc -o sql/docs.sql build-sql.xslt elements.xml
+xsltproc --xinclude -o sql/docs.sql build-sql.xslt elements.xml
